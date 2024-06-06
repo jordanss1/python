4a0b1485a1accc9ba00fb396f02530c4c69c6ca1
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2024-06-06 15:51:51 +0100

    initial

---
+++ hello.py
@@ -0,0 +1,37 @@
+# data = 10
+# print(type(data))
+
+# data = "10"
+# print(isinstance(data, str))
+
+#print(float(10))
+#print(float("10"))
+
+# first_string = "Hello"
+# second_string = first_string
+
+# print(first_string)
+# print(second_string)
+
+# first_string = "Hello World"
+
+# print(first_string)
+# print(second_string)
+
+# first_name = "david"
+# print(first_name.title())
+# print(first_name.upper())
+
+# first_name = "DAVID"
+# print(first_name.lower())
+
+# f_name = "david"
+# l_name = "grant"
+
+# full_name = f_name + " " + l_name
+# print(full_name.title())
+
+f_name = "david"
+age = 33
+
+print(f_name + " is " + str(age) + "years old")
